f1f169ca4f0abf0ee5dbdeca33c5528f054d8e66
umitao <umitao@gmail.com>
2020-06-13 12:03:39 +0300

    L1 E4 complete

--- src/Heading.js
+++ src/Heading.js
@@ -3,7 +3,7 @@ import React from "react";
 const Heading = () => {
   return (
     <header className="App-header">
-      CYF Hotel
+      Hotel Migracode
       <img
         className="App-logo"
         src="https://image.flaticon.com/icons/svg/139/139899.svg"
